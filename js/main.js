bd844c096b1a91b22e8018d329ea95c3bde8ec94
Rob Hitt <robhitt@gmail.com>
2017-04-16 17:20:41 -0400

    beta working deployment

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var jacksonButton = document.getElementById('btn');
+jacksonButton.addEventListener("click", addJackson);
+
+var albumsContainer = document.getElementById('album-cover');
+var url = "https://api.spotify.com/v1/search?q=artist:jackson 5&type=album";
+
+var albumCount = 0;
+
+function addJackson(){
+  var request = new XMLHttpRequest();
+
+  request.open('GET', url);
+
+  request.onload = function(){
+    var jacksonData = JSON.parse(request.responseText);
+    renderJackson(jacksonData);
+  };
+
+  request.send();
+}
+
+function renderJackson(data){
+  var newAlbum = '';
+  var albumURL = data.albums.items[albumCount].images[0].url;
+  newAlbum += "<img src='" + albumURL + "'>"
+  albumsContainer.insertAdjacentHTML('beforeend', newAlbum);
+  albumCount++;
+}
